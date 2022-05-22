adf0aae63a3c65bb84752a81db4aa45e595515e4
小澤　晶徳 <akinen@ozawaakiratokunoMacBook-Pro.local>
2022-05-22 20:51:15 +0900

    記事表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <HeroArea />
+  <div class="content">
+    <ul>
+      <li class="card" v-for="item in list" :key="item.slug">
+        <a :href="item.path">
+          <img class="card__img" :src="item.top_image" alt="">
+          <div>{{ item.title }}</div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+import Vue from 'vue'
+export default Vue.extend({
+  async asyncData({ $content }) {
+    const list = await $content({ deep: true }).fetch()
+    return { list }
+  },
+})
 </script>
+
+<style lang="stylus">
+.content
+  margin 0 auto
+  max-width 960px
+
+.card
+  width 100%
+  list-style none
+
+  &__img
+    width 100%
+</style>
